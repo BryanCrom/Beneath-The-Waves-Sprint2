0623b372937f77c4ad2efe493a0711d0214545d3
MaahirHussainShaik <142274442+MaahirHussainShaik@users.noreply.github.com>
2024-05-30 21:41:53 +1200

    Multi-player Setup
    Pun Setup and Player Movement in the lobby

---
+++ Beneath-the-Waves-Aryan-Tutorial/Assets/MultiPlayerScripts/RoomManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+     void Start()
+    {
+        Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    // Update is called once per frame
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+        public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+        Debug.Log("We are connected and in the room");
+    }
+
+    
+
+}
