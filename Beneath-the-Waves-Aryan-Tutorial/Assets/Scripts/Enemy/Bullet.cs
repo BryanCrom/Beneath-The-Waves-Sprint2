4a2cbff90c02545fe74950c4cae2c74378d19314
emsc111 <135845802+emsc111@users.noreply.github.com>
2024-06-04 19:21:32 +1200

    Restored Stashed changes
    Boss fixes

--- Beneath-the-Waves-Aryan-Tutorial/Assets/Scripts/Enemy/Bullet.cs
+++ Beneath-the-Waves-Aryan-Tutorial/Assets/Scripts/Enemy/Bullet.cs
@@ -49,5 +49,9 @@ public class Bullet : MonoBehaviour
             Debug.Log("Hit Wall: " + collision.gameObject.name);
             Destroy(gameObject);
         }
+        if (collision.gameObject.CompareTag("Boss"))
+        {
+            collision.gameObject.GetComponent<SchoolMovement>().takeDamage(1);
+        }
     }
 }
--- Beneath-the-Waves-Aryan-Tutorial/Assets/Scripts/Enemy/schoolMovement.cs
+++ Beneath-the-Waves-Aryan-Tutorial/Assets/Scripts/Enemy/schoolMovement.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class schoolMovement : MonoBehaviour
+public class SchoolMovement : MonoBehaviour
 {
-    public GameObject fishMissle;
     public float turnSpeed = 45.0f;
     public float forwardSpeed = 0.1f;
     public float smoothing = 0.8f;
     private float previousTurn = 0.0f;
-    public GameObject player;
-    private bool fired = false;
     private int health = 10;
-    private float scale = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +23,12 @@ public class schoolMovement : MonoBehaviour
         previousTurn = previousTurn * smoothing + turnSpeed * (Random.value * 2.0f - 1.0f) * (1 - smoothing);
         transform.Rotate(0.0f, previousTurn, 0.0f, Space.Self);
         transform.position += transform.forward * forwardSpeed;
-        //if (!fired) {
-            //FishM clone = Instantiate<FishM>(fishMissle);
-            //clone.setTarget(player);
-        //}
     }
     public void takeDamage(int damage)
     {
         health -= damage;
-        scale = (float)health / 10.0f;
-        transform.localScale = transform.localScale * scale;
+        transform.localScale = transform.localScale * (1f - ((10f - health) * 0.2f));
+        Debug.Log(health);
+        Debug.Log((1f - ((10f - health) * 0.2f)));
     }
 }
